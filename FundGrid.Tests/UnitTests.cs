81c07489cad14cb6b82fcdf76d80a276989e12a4
Heinrich Potgieter <DvorakUser@gmail.com>
2013-07-15 16:59:08 +0200

    Updated Unit Tests

--- FundGrid.Tests/UnitTests.cs
+++ FundGrid.Tests/UnitTests.cs
@@ -17,7 +17,7 @@ namespace FundGrid.Tests
         public void Given()
         {
             _ExistingGridItems = new List<GridItem>();
-            _ExistingGridItems.Add(new GridItem() {Amount = 50, Id = 10, Number = 4, Owner = "Someone1" });
+            _ExistingGridItems.Add(new GridItem() { Amount = 50, Id = 10, Number = 4, Owner = "Someone1" });
             _ExistingGridItems.Add(new GridItem() { Amount = 50, Id = 11, Number = 6, Owner = "Someone2" });
             _ExistingGridItems.Add(new GridItem() { Amount = 50, Id = 12, Number = 8, Owner = "Someone3" });
             _ExistingGridItems.Add(new GridItem() { Amount = 50, Id = 13, Number = 9, Owner = "Someone4" });
@@ -25,20 +25,26 @@ namespace FundGrid.Tests
             _ExistingGridItems.Add(new GridItem() { Amount = 50, Id = 15, Number = 24, Owner = "Someone6" });
         }
         [Test]
-        public void NullGridItemIsAvailible()
+        public void ZeroIdGridItemIsAvailibleForUser()
         {
             GridItem item = new GridItem() { Id = 0 };
             Assert.That(item.IsAvailible, Is.True);
         }
         [Test]
-        public void GridFilledNoData()
+        public void NonZeroIdGridItemIsNOTAvailibleForUser()
+        {
+            GridItem item = new GridItem() { Id = 10 };
+            Assert.That(item.IsAvailible, Is.False);
+        }
+        [Test]
+        public void GridWithNoExistingItemsShouldContainCorrectNumberOfItems()
         {
             Grid grid = new Grid() { DimensionColumns = 4, DimensionRows = 6, Id = 12, IncrementValue = 10, InitialValue = 10, ExistingGridItems = new List<GridItem>()};
             List<GridItem> ExistingGridItems = new List<GridItem>();
             Assert.That(grid.ItemCount, Is.EqualTo(24));
         }
         [Test]
-        public void GridFilledCorrectlyWithData()
+        public void GridWithExistingItemsShouldPlaceExistingItemsInCorrectPlaces()
         {
             Grid grid = new Grid() { DimensionColumns = 4, DimensionRows = 6, Id = 12, IncrementValue = 10, InitialValue = 10, ExistingGridItems = _ExistingGridItems };
             Assert.That(grid.FullGridItems[0][3].Number, Is.EqualTo(4));
