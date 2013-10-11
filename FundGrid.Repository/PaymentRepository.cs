0cc69c3bca373bb8927d927d8f8c90f63f8f24ac
unknown <DvorakUser@gmail.com>
2013-10-11 11:09:55 +0200

    Paypal backend commit

---
+++ FundGrid.Repository/PaymentRepository.cs
@@ -0,0 +1,45 @@
+﻿using log4net;
+using Moolah;
+using Moolah.PayPal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FundGrid.Repository
+{
+    public class PaymentRepository
+    {
+        private static readonly ILog _log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private PayPalConfiguration Configuration
+        {
+            get
+            {
+                var configuration = new PayPalConfiguration
+                                    (
+                                        PaymentEnvironment.Test,
+                                        "phillipk-facilitator_api1.solutionserver.co.za",
+                                        "1381410599",
+                                        "AFcWxV21C7fd0v3bYYYRCpSSRl31AL5rnfg2diW42wsppieMMGfCdXFg"
+                                    );
+                return configuration;
+            }
+        }
+        private PayPalExpressCheckout Gateway
+        {
+            get
+            {
+                return new PayPalExpressCheckout(Configuration);
+            }
+        }
+        public string CancelUrl { get; set; }
+        public string ConfirmationUrl { get; set; }
+        public string PayUsingPaypal(decimal amount)
+        {
+            var response = Gateway.SetExpressCheckout(amount, CurrencyCodeType.USD, CancelUrl, ConfirmationUrl);
+            if(response.Status == PaymentStatus.Failed)
+                _log.Error(String.Format("Payment of amount '{0}' failed", amount));
+            return response.RedirectUrl;
+        }
+    }
+}
---
+++ FundGrid.Tests/DevelopmentTool.cs
@@ -0,0 +1,21 @@
+﻿using FundGrid.Repository;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundGrid.Tests
+{
+    [TestFixture]
+    public class DevelopmentTool
+    {
+        [Test]
+        public void PayThroughPaypal()
+        {
+            var paypalRepo = new PaymentRepository();
+            var x = paypalRepo.PayUsingPaypal(100.00m);
+        }
+    }
+}
